11c103dabac1081ad93458b460eb4e0056b45833
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-07 10:51:00 -0600

    added JS query selector and moved script

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ let gold = 50;
 let currentWeapon = 0;
 // An array can be used to hold multiple values.
 let inventory = ["stick", "dagger", "sword"];
+
+//JavaScript interacts with the HTML using the Document Object Model, or DOM. The DOM is a tree of objects that represents the HTML. You can access the HTML using the document object, which represents your entire HTML document.
+//One method for finding specific elements in your HTML is using the querySelector() method. The querySelector() method takes a CSS selector as an argument and returns the first element that matches that selector.
+let button1 = document.querySelector("#button1");
