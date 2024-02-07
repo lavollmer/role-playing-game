9a450a875540a766fcff0df4f7b9f409f1f052cf
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-07 13:45:48 -0600

    consolidating code

--- script.js
+++ script.js
@@ -22,8 +22,28 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+//Arrays can store any data type. Objects are similar to arrays, but with a few differences. One difference is that objects use properties, or keys, to access and modify data.
+const locations = [
+  {
+    // Object properties are written as key: value pairs, where key is the name of the property (or the key), and value is the value that property holds.
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".',
+  },
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store.",
+  },
+];
 
-function goTown() {
+function update(location) {
   button1.innerText = "Go to store";
   button2.innerText = "Go to cave";
   button3.innerText = "Fight dragon";
@@ -31,19 +51,37 @@ function goTown() {
   button2.onclick = goCave;
   button3.onclick = fightDragon;
   text.innerText =
-    "You are in the town square. You see a sign that says Store.";
+    'You are in the town square. You see a sign that says "Store".';
 }
 
-//The innerText property controls the text that appears in an HTML element.
-function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
-  text.innerText = "You enter the store.";
+//update function will use data from the location passed through
+function goTown() {
+  update();
 }
+
+function goStore() {}
+
+// function goTown() {
+//   button1.innerText = "Go to store";
+//   button2.innerText = "Go to cave";
+//   button3.innerText = "Fight dragon";
+//   button1.onclick = goStore;
+//   button2.onclick = goCave;
+//   button3.onclick = fightDragon;
+//   text.innerText =
+//     'You are in the town square. You see a sign that says "/Store"/.';
+// }
+
+// //The innerText property controls the text that appears in an HTML element.
+// function goStore() {
+//   button1.innerText = "Buy 10 health (10 gold)";
+//   button2.innerText = "Buy weapon (30 gold)";
+//   button3.innerText = "Go to town square";
+//   button1.onclick = buyHealth;
+//   button2.onclick = buyWeapon;
+//   button3.onclick = goTown;
+//   text.innerText = "You enter the store.";
+// }
 
 function goCave() {
   console.log("Going to cave.");
@@ -62,3 +100,5 @@ function buyWeapon() {}
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+
+function update(location) {}
