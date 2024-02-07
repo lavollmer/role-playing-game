52cbb401de4eae7f7637b78a20b4b960d7f1b8e5
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-07 10:38:16 -0600

    setting variables for JS file

---
+++ script.js
@@ -0,0 +1,6 @@
+//The let keyword tells JavaScript you are declaring a variable.
+//Variables can be assigned a value. When you do this while you declare it, this is called initialization.
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
