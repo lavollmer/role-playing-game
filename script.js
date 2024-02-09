c9811c318f3667347a92d43945b06bd2d551ab4a
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-09 10:44:21 -0600

    updating buyWeapon function pt.2

--- script.js
+++ script.js
@@ -98,7 +98,13 @@ function buyHealth() {
   }
 }
 
-function buyWeapon() {}
+function buyWeapon() {
+  if (gold >= 30) {
+    gold -= 30;
+    currentWeapon++;
+    goldText.innerText = gold;
+    text.innerText = "You now have a new weapon.";
+}
 
 function fightSlime() {}
 
