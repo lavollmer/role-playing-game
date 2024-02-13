9872a2e0fc800db3decef971c163e27e22c43f42
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-02-13 15:34:04 -0600

    finalizing function attack

--- script.js
+++ script.js
@@ -195,6 +195,8 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText +=
     " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -= weapons[currentWeapon].power;
 }
 
 function dodge() {}
