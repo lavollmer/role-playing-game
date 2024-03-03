551e824a672f90d503f2d6c79aa195dcfd0ab36d
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-03 10:15:19 -0600

    cleaning up code

--- script.js
+++ script.js
@@ -77,16 +77,6 @@ const locations = [
     "button functions": [goTown, goTown, goTown],
     text: "The monster screams Arg! as it dies. You gain experience points and find gold.",
   },
-  {
-    name: "kill monster",
-    "button text": [
-      "Go to town square",
-      "Go to town square",
-      "Go to town square",
-    ],
-    "button functions": [goTown, goTown, goTown],
-    text: "The monster screams Arg! as it dies. You gain experience points and find gold.",
-  },
 ];
 
 function update(location) {
