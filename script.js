6074df2d532127b559ef4b6af90877358f7059ce
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-04 16:07:13 -0600

    updating attack function

--- script.js
+++ script.js
@@ -241,8 +241,9 @@ function attack() {
     defeatMonster();
   }
 
-  if (Math.random() <= 0.1) {
+  if (Math.random() <= 0.1 && inventory.length !== 1) {
     text.innerText += " Your " + inventory.pop() + " breaks.";
+    currentWeapon--;
   }
 }
 
