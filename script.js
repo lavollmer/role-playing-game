89661494c99230eca069ce4b868459dabe54cd31
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-04 22:28:49 -0600

    Added easter egg

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ const locations = [
       "Go to town square",
       "Go to town square",
     ],
-    "button functions": [goTown, goTown, goTown],
+    "button functions": [goTown, goTown, easterEgg],
     text: "The monster screams Arg! as it dies. You gain experience points and find gold.",
   },
   {
@@ -326,5 +326,9 @@ function pick(guess) {
     text.innerText += "Wrong! You lose 10 health!";
     health -= 10;
     healthText.innerText = health;
+    if (health <= 0) {
+      return lose();
+      ``;
+    }
   }
 }
